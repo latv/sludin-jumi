f870dea1c28a89b1e8b782f78bf59a632d7c267a
Janis Feldmanis <feldmanis45@gmail.com>
2021-03-09 15:18:59 +0200

    added picture to upload

--- addposter.php
+++ addposter.php
@@ -1,10 +1,53 @@
 <?php
 if ((isset($_POST['submit']))) { 
  
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  
+  // Check if image file is a actual image or fake image
+  if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+    } else {
+      echo "File is not an image.";
+      $uploadOk = 0;
+    }
+  }
+  
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+  
+  
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+  }
+  
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
+  }
     
-    
-    $sql = "INSERT INTO poster (user_id, category, text, phone_number, selected_date)
-    VALUES ('".$_SESSION["userid"]."', '".$_POST["category"]."', '".$_POST["text"]."', '".$_POST["phone_number"]."', '".$_POST["term"]."')";
+    $sql = "INSERT INTO poster (user_id, category, text, phone_number, selected_date,image )
+    VALUES ('".$_SESSION["userid"]."', '".$_POST["category"]."', '".$_POST["text"]."', '".$_POST["phone_number"]."', '".
+    $_POST["term"]."' ,  '".
+    basename($_FILES["fileToUpload"]["name"])."')";
     
     if ($conn->query($sql) === TRUE) {
       echo "sludinajums veiksmīgi pievienots!<br><br>";
@@ -18,7 +61,7 @@ if ((isset($_POST['submit']))) {
 
 
 
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
 
 kategorijas: <select name="category" id="category">
 
@@ -40,6 +83,9 @@ if ($result->num_rows > 0) {
 ?>
 </select>
 <br>
+Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload"><br>
+
 teksts: <textarea type="text" name="text"></textarea><br>
 telefona numurs: <input type="number" name="phone_number"><br>
 termiņš   <select id="term" name="term">
